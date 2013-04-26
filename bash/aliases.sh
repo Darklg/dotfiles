58cbe3c4a2f1e29aecd6063aa64aa1c4ca222dcd
Darklg <kev.rocher@gmail.com>
2013-04-26 20:41:41 +0200

    Bash aliases

---
+++ bash/aliases.sh
@@ -0,0 +1,25 @@
+
+# Directory
+alias ls='ls -Gh';
+alias ll='ls -l';
+alias la='ls -a';
+alias lla='ls -al';
+alias ..='cd ..';
+alias ...='cd ../..';
+alias hosts='sudo vim /etc/hosts';
+
+# Files
+alias clean='find . -iname ".DS_Store" -delete';
+
+# Dates
+alias now='date +"%T';
+alias nowdate='date +"%d-%m-%Y"';
+
+# reboot / halt / poweroff
+alias reboot='sudo /sbin/reboot';
+alias poweroff='sudo /sbin/poweroff';
+alias halt='sudo /sbin/halt';
+alias shutdown='sudo /sbin/shutdown';
+
+# Thanks to
+# - http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
