d441d253821266190267302296ac0f0ec059b7b7
Darklg <kev.rocher@gmail.com>
2013-05-08 11:40:13 +0200

    New aliases for Git

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -26,10 +26,11 @@ alias shutdown='sudo /sbin/shutdown';
 alias suspend='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Resources/CGSession -suspend'
 
 # Alias Git
-alias gs='git status -s && git fetch'
 alias ga='git add -u . && git add .'
-alias gitput='git stash && git pull && git stash apply && git stash drop && git push'
-alias gitbye='git update-index --assume-unchanged'
+alias gfoire='git reset --hard HEAD'
+alias gitput='git stash && git pull && git push && git stash apply && git stash drop'
+alias gl='git log --pretty=format:"%h %ad | %s%d [%an]" --max-count=20 --graph --date=short'
+alias gs='git status -s && git fetch'
 
 # Thanks to
 # - http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
