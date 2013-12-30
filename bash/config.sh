2cba9b9b69c3dcc41237cbb517bb04255c25828d
Darklg <kev.rocher@gmail.com>
2013-12-30 11:38:14 +0100

    Config - adds sh & config & actions when terminal opens

--- bash/config.sh
+++ bash/config.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#!/bin/sh
+
+#-------------------------
+# Conf
 
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;36m\] \t \w\n\$ \[\033[00m\]';
 export COPYFILE_DISABLE=true;
+
+# don't put duplicate lines in the history
+export HISTCONTROL=ignoredups;
+
+#-------------------------
+# Actions when terminal opens
+
+# Display uptime
+uptime;
