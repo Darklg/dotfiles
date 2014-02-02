b5c872639eae2ad5062b6d7da06fc55a08149776
Darklg <kev.rocher@gmail.com>
2014-02-02 23:06:11 +0100

    Fix some git aliases & better clean & quick webserver

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Functions
 function mkdircd () { mkdir -p "$@" && eval cd "\"\$$#\""; }
 
 # Directory
@@ -11,8 +12,12 @@ alias ..='cd ..';
 alias ...='cd ../..';
 alias hosts='sudo vim /etc/hosts';
 
+# Utilities
+# Quick Webserver
+alias serv='open http://0.0.0.0:1234 && python -m SimpleHTTPServer 1234';
+
 # Files
-alias clean='find . -iname ".DS_Store" -delete';
+alias clean='find . -iname ".DS_Store" -delete && dot_clean .';
 
 # Paste clipboard in new vim window | apple.stackexchange.com/a/1533
 alias pavi='pbpaste | vim -';
@@ -34,11 +39,11 @@ alias suspend='/System/Library/CoreServices/"Menu Extras"/User.menu/Contents/Res
 # Alias Git
 alias ga='git add -u . && git add .'
 alias gfoire='git reset --hard HEAD'
-alias gitput='git stash && git pull && git push && git stash apply && git stash drop'
+alias gitput='git stash && git pull && git push --all && git stash apply && git stash drop'
 alias gl='git log --pretty=format:"%h %ad | %s%d [%an]" --max-count=20 --graph --date=short'
 alias gs='git status -s && git fetch'
 alias ghpages='git checkout gh-pages && git rebase master && git checkout master';
-alias giti='git gc && git pull --rebase && git push';
+alias giti='git gc && git pull --rebase && git push --all';
 
 # Thanks to
 # - http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
