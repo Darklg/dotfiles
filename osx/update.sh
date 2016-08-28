655724e41313a4d80e704a5adfe7859ef853adf1
Darklg <kev.rocher@gmail.com>
2016-08-28 23:23:45 +0200

    Update - remove cask & mackup auto backup

--- osx/update.sh
+++ osx/update.sh
@@ -26,28 +26,8 @@ gem update;
 brew update;
 brew upgrade;
 
-# CASK
-export HOMEBREW_CASK_OPTS=--appdir=/Applications;
-casks=( $(brew cask list) )
-
-for cask in ${casks[@]}
-do
-    # in the first line there is version
-    current="$(brew cask info $cask | sed -n '1p' | sed -n 's/^.*: \(.*\)$/\1/p')"
-    installed=( $(ls /opt/homebrew-cask/Caskroom/$cask))
-    if (! [[ " ${installed[@]} " == *" $current "* ]]); then
-        (set -x; brew cask install $cask --force;)
-    fi
-done
-
 # CLEAN
 brew cleanup;
 brew cask cleanup;
 brew doctor;
-
-###################################
-## Backups
-###################################
-
-mackup backup;
 
