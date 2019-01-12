b14acfd5f6b0a7a456c2fcc45b21f60a1da57bdd
Kevin <kevin@rocher-dev.com>
2019-01-12 16:38:06 +0100

    OSX - Quick way of opening the apps needing to be up-to-date.

---
+++ osx/open.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###################################
+## Open Apps
+###################################
+
+open /Applications/Reeder.app;
+open /Applications/Pocket.app;
+open /Applications/Spotify.app;
+open /Applications/Mail.app;
