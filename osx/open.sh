5bb934587f0598c987483ff83531fbb4b66c0af5
Kevin <kevin@rocher-dev.com>
2019-05-03 18:58:40 +0200

    add new apps to open

--- osx/open.sh
+++ osx/open.sh
@@ -4,8 +4,12 @@
 ## Open Apps
 ###################################
 
+open /Applications/Photos.app;
 open /Applications/Reeder.app;
 open /Applications/Pocket.app;
 open /Applications/Spotify.app;
 open /Applications/Mail.app;
 open /Applications/Dash.app;
+open /Applications/Tower.app;
+open /Applications/Todoist.app;
+open /Applications/Trello.app;
