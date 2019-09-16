38b10d2bac53475ae6ea171647bd54dd504cb789
Kevin <kevin@rocher-dev.com>
2019-09-16 19:30:39 +0200

    Bash Windows - Change monitor ID to avoid an OSX bug

--- bash/windows.sh
+++ bash/windows.sh
@@ -46,21 +46,24 @@ end tell
 EOF
 }
 
+dotfiles_position_left_monitor_id=3;
+dotfiles_position_right_monitor_id=2;
+
 ###################################
 ## Position the apps
 ###################################
 
 # Left Screen
-dotfiles_position_app "Notes" 3 "move and resize to {0, 0, 640, 708}";
-dotfiles_position_app "Terminal" 3 "move and resize to {640, 0, 640, 708}";
-dotfiles_position_app "Tower" 3 "do action Lower Left";
-dotfiles_position_app "Google Chrome" 3 "do action Right";
+dotfiles_position_app "Notes" ${dotfiles_position_left_monitor_id} "move and resize to {0, 0, 640, 708}";
+dotfiles_position_app "Terminal" ${dotfiles_position_left_monitor_id} "move and resize to {640, 0, 640, 708}";
+dotfiles_position_app "Tower" ${dotfiles_position_left_monitor_id} "do action Lower Left";
+dotfiles_position_app "Google Chrome" ${dotfiles_position_left_monitor_id} "do action Right";
 
 # Right Screen
-dotfiles_position_app "Mail" 2 "do action Left";
-dotfiles_position_app "Todoist" 2 "move and resize to {1280, 0, 640, 708}";
-dotfiles_position_app "Trello" 2 "move and resize to {1920, 0, 640, 708}";
-dotfiles_position_app "Slack" 2 "do action Lower Right";
+dotfiles_position_app "Mail" ${dotfiles_position_right_monitor_id} "do action Left";
+dotfiles_position_app "Todoist" ${dotfiles_position_right_monitor_id} "move and resize to {1280, 0, 640, 708}";
+dotfiles_position_app "Trello" ${dotfiles_position_right_monitor_id} "move and resize to {1920, 0, 640, 708}";
+dotfiles_position_app "Slack" ${dotfiles_position_right_monitor_id} "do action Lower Right";
 
 # Middle Screen
 dotfiles_position_app "Sublime Text" 1 "do action Full Screen";
