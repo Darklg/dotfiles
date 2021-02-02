b6d9165c68aa28c626b7fe180ae187cfb193608b
Kevin <kevin@rocher-dev.com>
2021-02-02 15:17:58 +0100

    sandbox php  & checks

--- osx/sandbox.sh
+++ osx/sandbox.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 MAINDIR="/Users/kevin/Websites/htdocs/localhost";
+if [[ "${1}" == '' ]];then
+    echo "Missing the dirname";
+    return 0;
+fi;
+if [[ -d "${MAINDIR}/${1}/" ]];then
+    echo "Dir already exists";
+    return 0;
+fi;
 mkdir "${MAINDIR}/${1}/";
 cd "${MAINDIR}/${1}/";
-touch "${MAINDIR}/${1}/index.html";
+touch "${MAINDIR}/${1}/index.php";
 subl "${MAINDIR}/${1}/";
-open "http://localhost/${1}/index.html";
+open "http://localhost/${1}/index.php";
