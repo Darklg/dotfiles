ba95c7ba29cb6a2852679fc46f76d9af1f367f25
Kevin <kevin@rocher-dev.com>
2021-02-05 19:11:17 +0100

    Defaults OSX - force Bash

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -6,6 +6,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+## Force Bash
+chsh -s /bin/bash
+
 ##
 ## MAINTENANCE ##
 
