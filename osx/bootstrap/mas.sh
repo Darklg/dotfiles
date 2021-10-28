70710a41c006139508848085257b4ce80d52cad3
Kevin <kevin@rocher-dev.com>
2021-10-28 23:23:09 +0200

    Update apps

--- osx/bootstrap/mas.sh
+++ osx/bootstrap/mas.sh
@@ -6,13 +6,18 @@
 
 mas install 1147396723; # Whatsapp
 mas install 1278508951; # Trello
+mas install 1289583905  # Pixelmator
 mas install 1384080005; # Tweetbot
 mas install 1388020431; # DevCleaner
+mas install 1425368544  # Timery
 mas install 1449412482; # Reeder
+mas install 1518425043  # Boop
 mas install 1518425043; # Boop
+mas install 1568924476  # Mela
+mas install 411643860   # Daisy Disk
 mas install 513610341;  # Integrity
 mas install 568494494;  # Pocket
 mas install 585829637;  # Todoist
+mas install 639968404;  # Parcel
 mas install 669840342;  # Cashflows
 mas install 957734279;  # TogglDesktop
-mas install 1518036000  # Sequel Ace
