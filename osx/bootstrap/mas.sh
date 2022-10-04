206dc7a4a7d08637c972fb56ed41396736923f7e
Kevin <kevin@rocher-dev.com>
2022-10-04 11:20:16 +0200

    Bootstrap MAS - update list

--- osx/bootstrap/mas.sh
+++ osx/bootstrap/mas.sh
@@ -4,20 +4,35 @@
 ## Mac App Store
 ###################################
 
+mas install 1136220934; # Infuse
 mas install 1147396723; # Whatsapp
 mas install 1278508951; # Trello
 mas install 1289583905  # Pixelmator
+mas install 1357379892; # Menu Bar Controller for Sonos
 mas install 1384080005; # Tweetbot
 mas install 1388020431; # DevCleaner
 mas install 1425368544  # Timery
-mas install 1449412482; # Reeder
-mas install 1518425043  # Boop
+mas install 1480068668; # Messenger
 mas install 1518425043; # Boop
-mas install 1568924476  # Mela
-mas install 411643860   # Daisy Disk
+mas install 1529448980; # Reeder
+mas install 1568924476; # Mela
+mas install 1569600264; # Pandan
+mas install 1596283165; # rcmd
+mas install 409183694;  # Keynote
+mas install 409201541;  # Pages
+mas install 409203825;  # Numbers
+mas install 411643860;  # Daisy Disk
+mas install 411643860;  # DaisyDisk
+mas install 462054704;  # Microsoft Word
+mas install 462058435;  # Microsoft Excel
 mas install 513610341;  # Integrity
+mas install 545519333;  # Prime Video
 mas install 568494494;  # Pocket
 mas install 585829637;  # Todoist
 mas install 639968404;  # Parcel
+mas install 640199958;  # Developer
 mas install 669840342;  # Cashflows
+mas install 784466108;  # exifpurge
+mas install 897118787;  # Shazam
+mas install 899247664;  # TestFlight
 mas install 957734279;  # TogglDesktop
