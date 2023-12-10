5cf07e1d968c061d9598b0a954d6de1aa9f3e24d
Kevin <kevin@rocher-dev.com>
2023-12-10 21:19:04 +0100

    raycast - add kmeet quick open

---
+++ raycast-commands/get-kmeet-link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Open kmeet
+# @raycast.mode silent
+# @raycast.packageName Infomaniak
+# @raycast.icon images/kmeet.png
+# @raycast.description Start a kmeet session
+
+open https://kmeet.infomaniak.com/?lang=fr#action=create
