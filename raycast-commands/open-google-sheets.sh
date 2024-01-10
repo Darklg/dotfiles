036e131c0e066844a4b542f560b193642b724c28
Kevin <kevin@rocher-dev.com>
2024-01-10 17:53:25 +0100

    Raycast action - open google sheets

---
+++ raycast-commands/open-google-sheets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Google Sheets
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+
+# Optional parameters:
+# @raycast.icon images/google-sheets.png
+
+open "https://sheets.new";
