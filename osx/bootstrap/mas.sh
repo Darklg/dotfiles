2dc378b05cccea058a0b3de21bb35f215becca25
Kevin <kevin@rocher-dev.com>
2024-11-19 10:24:30 +0100

    MAS - Apps

--- osx/bootstrap/mas.sh
+++ osx/bootstrap/mas.sh
@@ -10,16 +10,21 @@ mas install 1147396723; # Whatsapp
 mas install 1278508951; # Trello
 mas install 1289583905; # Pixelmator
 mas install 1357379892; # Menu Bar Controller for Sonos
+mas install 1358823008; # Flighty
 mas install 1388020431; # DevCleaner
 mas install 1425368544; # Timery
 mas install 1452453066; # Hidden Bar
 mas install 1480068668; # Messenger
+mas install 1486015273; # AS Timer
 mas install 1518036000; # Sequel Ace
 mas install 1518425043; # Boop
+mas install 1522267256; # Shareful
 mas install 1529448980; # Reeder
 mas install 1568924476; # Mela
 mas install 1569600264; # Pandan
+mas install 1569813296; # 1pass for Safari
 mas install 1596283165; # rcmd
+mas install 1606145041; # Sleeve
 mas install 409183694;  # Keynote
 mas install 409201541;  # Pages
 mas install 409203825;  # Numbers
@@ -32,8 +37,8 @@ mas install 568494494;  # Pocket
 mas install 585829637;  # Todoist
 mas install 639968404;  # Parcel
 mas install 640199958;  # Developer
+mas install 6444602274; # Ivory
 mas install 669840342;  # Cashflows
-mas install 784466108;  # exifpurge
 mas install 803453959;  # Slack
 mas install 897118787;  # Shazam
 mas install 899247664;  # TestFlight
