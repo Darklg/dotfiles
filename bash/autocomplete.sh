02d4f0ee4a53ff100582e9f5e5c770debe1f6a53
Kevin <kevin@rocher-dev.com>
2025-02-13 13:42:26 +0100

    Autocomplete : add ssh config list

--- bash/autocomplete.sh
+++ bash/autocomplete.sh
@@ -11,3 +11,27 @@ complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes Syste
 ###################################
 
 complete -o "nospace" -W "add bisect branch checkout clone commit diff fetch grep init log merge mv pull push rebase reset rm show status submodule tag" git;
+
+###################################
+## SSH
+###################################
+
+_ssh_hosts_completion() {
+    if [[ "${DKLG_SSH_CONFIG_FILE}" == "" ]];then
+        return 0;
+    fi;
+
+    COMPREPLY=()
+    local cur prev
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    if [ "$prev" = "ssh" ]; then
+        local hosts
+        hosts=$(grep -E '^[ \t]*Host +[^*]' ${DKLG_SSH_CONFIG_FILE}  2>/dev/null | sed -e 's/^[ \t]*Host[ \t]*//')
+        COMPREPLY=( $(compgen -W "${hosts}" -- "${cur}") )
+    fi
+    return 0
+}
+
+complete -F _ssh_hosts_completion ssh
