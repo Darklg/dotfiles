003d91eb4672fdf04dfc159c718fb74de0539c1d
Kevin <kevin@rocher-dev.com>
2025-03-20 21:30:15 +0100

    Raycast - add sutom command

---
+++ raycast-commands/sutom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sutom
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Files
+open -a "Safari" "https://framed.wtf/";
+open -a "Safari" "https://framed.wtf/one-frame";
+open -a "Safari" "https://quordle.desmaths.fr/";
+open -a "Safari" "https://wordle.louan.me";
+open -a "Safari" "https://sutom.nocle.fr";
+open -a "Safari" "https://travle.earth";
+open -a "Safari" "https://travle.earth/fra";
+open -a "Safari" "https://worldle.teuteuf.fr";
+open -a "Safari" "https://sumplete.com";
+
+# Task
+open -a "Safari" https://app.todoist.com/app/task/motus-66xRVC49gQvRVwqX
+
+# Slack
+open "slack://channel?team=T85TA7R29&id=C02TD83TTRB";
+
+# Sticky
+open "/System/Applications/Stickies.app";
