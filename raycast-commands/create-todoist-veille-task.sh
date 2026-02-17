35168079b7ed92769613a4b1afcc481d027f4a59
Kevin <kevin@rocher-dev.com>
2026-02-17 10:43:31 +0100

    Raycast action - use url scheme for todoist

--- raycast-commands/create-todoist-veille-task.sh
+++ raycast-commands/create-todoist-veille-task.sh
@@ -13,29 +13,9 @@
 # Documentation:
 # @raycast.description Create a task in Todoist with specific tags
 
-_TEMP_ID=$(uuidgen);
-_UUID_TMP=$(uuidgen);
-_config_file="$(dirname $BASH_SOURCE)/raycast-config.sh";
-
-if ! command -v op &> /dev/null; then
-  echo "op is not installed. Please install op to access 1Password."
-  return 0;
-fi
-
-# Get the Todoist private token from 1Password
-eval $(op signin);
-_todoist_token=$(op item get --reveal "Todoist" --field "TOKEN_API");
-_todoist_labels="[\"veille-actionnable\",\"nobrains\",\"commitdujour\"]";
 
 # Replace straight quotes with typographic quotes in the input
 _task_content=$(echo "${1}" | sed "s/\"/“/g; s/'/‘/g")
-
-echo "${1}" >> ~/raycast-send-todoist-history.txt;
-
-curl -s https://api.todoist.com/sync/v9/sync \
-    -H "Authorization: Bearer ${_todoist_token}" \
-    -d commands="[{\"type\": \"item_add\", \"due\":{\"string\":\"tomorrow\"},\"uuid\": \"${_UUID_TMP}\",\"temp_id\": \"${_TEMP_ID}\", \"args\": {\"content\": \"${_task_content}\",\"labels\": ${_todoist_labels}}}]" > /dev/null;
-
-open "todoist://inbox";
-
+open "todoist://openquickadd?content=${_task_content} @veille-actionnable @nobrains @commitdujour";
 echo "Task created !";
+echo "${1}" >> ~/raycast-send-todoist-history.txt;
