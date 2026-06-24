010c50ac14cd3f800319eec5523c05c89d18bc85
Kevin <kevin@rocher-dev.com>
2026-06-24 14:31:46 +0200

    Update MAS

--- osx/bootstrap/mas.sh
+++ osx/bootstrap/mas.sh
@@ -4,52 +4,56 @@
 ## Mac App Store
 ###################################
 
-# mas install 1384080005; # Tweetbot
 mas install 1055511498; # Day One
 mas install 1099120373; # Exporter
 mas install 1111570163; # GrandPerspective
 mas install 1136220934; # Infuse
 mas install 1278508951; # Trello
-mas install 1289583905; # Pixelmator
+mas install 1289583905; # Pixelmator Pro
 mas install 1291898086; # Toggl Track
-mas install 1358823008; # Flighty
 mas install 1365531024; # 1Blocker
 mas install 1388020431; # DevCleaner
 mas install 1425368544; # Timery
-mas install 1452453066; # Hidden Bar
 mas install 1463298887; # Userscripts
+mas install 1475387142; # Tailscale
 mas install 1480068668; # Messenger
-mas install 1486015273; # AS Timer
-mas install 1507361706; # Hicoffee
+mas install 1486015273; # AS TimerPRO
+mas install 1507361706; # HiCoffee
 mas install 1518036000; # Sequel Ace
-mas install 1518425043; # Boop
 mas install 1522267256; # Shareful
 mas install 1529448980; # Reeder
-mas install 1561732866; # Hiwater
+mas install 1561732866; # AIWater
 mas install 1568924476; # Mela
 mas install 1569600264; # Pandan
 mas install 1569813296; # 1Password for Safari
 mas install 1579200037; # Medis
 mas install 1594183810; # Shortery
+mas install 1594420480; # Prompt
 mas install 1604176982; # One Thing
+mas install 1663047912; # Screens 5
 mas install 302584613;  # Kindle
 mas install 310633997;  # WhatsApp
+mas install 361285480;  # Keynote
+mas install 361304891;  # Numbers
+mas install 361309726;  # Pages
+mas install 375589283;  # Parcel
 mas install 403304796;  # iNet Network Scanner
-mas install 409183694;  # Keynote
-mas install 409201541;  # Pages
-mas install 409203825;  # Numbers
 mas install 411643860;  # DaisyDisk
+mas install 457622435;  # Yoink
 mas install 472226235;  # LanScan
 mas install 497799835;  # Xcode
-mas install 512464723;  # AS Timer
 mas install 513610341;  # Integrity
-mas install 545519333;  # Prime Video
 mas install 585829637;  # Todoist
-mas install 639968404;  # Parcel
 mas install 640199958;  # Developer
-mas install 6444602274; # Ivory
+mas install 6502996298; # No Youtube Shorts
 mas install 6504709660; # Online Check
+mas install 669840342;  # Cashflows
+mas install 6720708363; # Obsidian Web Clipper
+mas install 6747410609; # Quick Subtitles
+mas install 6753110395; # Folder Quick Look
+mas install 6768724888; # Trace
+mas install 747648890;  # Telegram
 mas install 803453959;  # Slack
+mas install 847809913;  # Download Shuttle
 mas install 897118787;  # Shazam
 mas install 899247664;  # TestFlight
-mas install 957734279;  # TogglDesktop
